8bc774d2dd1894cf7b8e913ab494599d29c21994
vladi <vladi@FELPY>
2023-03-09 23:23:19 +0600

    Добавьте файлы проекта.

---
+++ EncryptDecrypt/EncryptDecrypt.cpp
@@ -0,0 +1,63 @@
+﻿#include <Windows.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Шифрование
+string encrypt(string input) {
+    vector<char> word(input.begin(), input.end());
+    string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    for (int i = 0; i < (int)input.length(); i++) {
+        for (int j = 0; j < (int)alphabet.length(); j++) {
+            if (word[i] == alphabet[j]) {
+                word[i] = alphabet[(j + 3) % 26];
+
+                break;
+            }
+        }
+    }
+    string str(word.begin(), word.end());
+    return str;
+}
+
+// Расшифровка
+string decrypt(string input) {
+    vector<char> word(input.begin(), input.end());
+    string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    for (int i = 0; i < (int)input.length(); i++) {
+        for (int j = 0; j < (int)alphabet.length(); j++) {
+            if (word[i] == alphabet[j]) {
+                word[i] = alphabet[(j - 3) % 26];
+
+                break;
+            }
+        }
+    }
+    string str(word.begin(), word.end());
+    return str;
+}
+
+
+int main() {
+
+    //Обычный вид без шифрования
+    string text = "What are u doing dude?";
+    cout << text << endl;
+
+    //Шифрование
+    string textencrypt = encrypt(text);
+    cout << textencrypt << endl;
+
+    string url = encrypt("https://ya.ru/");
+    cout << url << endl;
+
+    //Расшифровка
+    string textdecrypt = decrypt(textencrypt);
+    cout << textdecrypt << endl;
+
+
+
+
+}
